d970eb12a9f95e2ae7b76735da21a6f292c0452f
Ahmad <ahmad.alsayed@dci-student.org>
2024-11-26 13:39:31 +0100

    addingSomeComments

--- index.js
+++ index.js
@@ -1,88 +1,115 @@
-class Calculator{
+class Calculator {
+  constructor() {
+    this.PI = Math.PI;
+    this.E = Math.E;
+  }
+  /**
+   * calculating the ratio
+   * @param {Number} x
+   * @param {Number} y
+   * @param {Number} width
+   * @returns  Number
+   */
+  ratio(x, y, width) {
+    return (width * y) / x;
+  }
+  /**
+   * calculating the Percentage
+   * @param {Number} x
+   * @param {Number} y
+   * @returns  Number
+   */
+  percentage(x, y) {
+    const toPercentage = (x / y) * 100;
+    return y === 0 ? "Error:Divisor cannot be zero " : `${toPercentage}`;
+  }
+  /**
+   * calculating the Sum
+   * @param {Number} x
+   * @param {Number} y
+   * @returns  Sum
+   */
+  add(x, y) {
+    return x + y;
+  }
 
-    constructor(){
-      this.PI=Math.PI
-      this.E=Math.E
+  /**
+   * calculating the Products
+   * @param {Number} x
+   * @param {Number} y
+   * @returns  Number
+   */
+  multiply(x, y) {
+    return x * y;
+  }
+
+  /**
+   * dividing two numbers
+   * @param {Number} x
+   * @param {Number} y
+   * @returns  Number
+   */
+  divide(x, y) {
+    if (y !== 0) {
+      return x / y;
+    } else {
+      return `Error: Divisor cannot be zero`;
     }
-
-    ratio(x,y,width){
-
-       
-
-
-        return (width*y)/x
-     
+  }
+  /**
+   * dividing two numbers and give back remainder
+   * @param {Number} x
+   * @param {Number} y
+   * @returns  Number
+   */
+  remainder(x, y) {
+    if (y !== 0) {
+      return x % y;
+    } else {
+      return `Error: Divisor cannot be zero`;
     }
-
-
-
-
-    percentage(x,y){
-     const toPercentage=(x / y) * 100
-     return y===0?'Error:Divisor cannot be zero ':`${toPercentage}`
+  }
+  /**
+   * expand  the x by y
+   * @param {Number} x
+   * @param {Number} y
+   * @returns  Number
+   */
+  elevate(x, y) {
+    return x ** y;
+  }
+  /**
+   * calculating the root of the  x
+   * @param {Number} x
+   * @param {Number} y
+   * @returns  Number
+   */
+  sqrt(x) {
+    if (x > 0) {
+      return Math.sqrt(x);
+    } else {
+      return `Error: Cannot calculate the square root of a negative number.`;
     }
-
- 
-
-    add(x,y){
-       return x+y
-   
-    multiply(x, y) {
-      return x * y;
-    }
-
-    divide(x, y) {
-      if (y !== 0) {
-        return x / y;
-      } else {
-        return `Error: Divisor cannot be zero`
-      }
-    }
-
-    remainder(x, y) {
-      if (y !== 0) {
-        return x % y;
-      } else {
-        return `Error: Divisor cannot be zero`
-      }
-    }
-
-    elevate(x, y) {
-      return x ** y;
-    }
-
-    sqrt(x) {
-      if (x > 0) {
-        return Math.sqrt(x);
-      } else {
-        return `Error: Cannot calculate the square root of a negative number.`
-      }
-
-    }
-   subtract(x,y){
-    return y - x
+  }
+  /**
+   * subtracting two numbers
+   * @param {Number} x
+   * @param {Number} y
+   * @returns  Number
+   */
+  subtract(x, y) {
+    return y - x;
+  }
 }
 
+const calc = new Calculator();
 
-}
-
-const calc=new Calculator()
-
-console.log(calc.ratio(5,7,300));
-console.log(calc.percentage(3,12));
-console.log(calc.add(5,7));
-console.log(calc.subtract(5,7));
-
-
-//console.log(calc.ratio(5, 7, 300));
-//console.log(calc.percentage(3, 12)); 
-//console.log(calc.add(5, 7)); 
-//console.log(calc.subtract(5, 7)); 
-console.log(calc.multiply(5, 7)); 
-console.log(calc.divide(35, 7)); 
-console.log(calc.remainder(7, 5)); 
-console.log(calc.elevate(5, 3)); 
+console.log(calc.ratio(5, 7, 300));
+console.log(calc.percentage(3, 12));
+console.log(calc.add(5, 7));
+console.log(calc.subtract(5, 7));
+console.log(calc.multiply(5, 7));
+console.log(calc.divide(35, 7));
+console.log(calc.remainder(7, 5));
+console.log(calc.elevate(5, 3));
 console.log(calc.sqrt(25));
-
-
-
