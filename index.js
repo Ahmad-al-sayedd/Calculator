dbe5130ab672bd5038baf307a3aa0b268f016fc1
Judith Bohmann <judith.bohmann@dci-student.org>
2024-11-26 11:38:46 +0100

    add methods

--- index.js
+++ index.js
@@ -4,8 +4,51 @@ class Calculator{
       this.PI=Math.PI
       this.E=Math.E
     }
-    add(){
+   
+    multiply(x, y) {
+      return x * y;
+    }
 
+    divide(x, y) {
+      if (y !== 0) {
+        return x / y;
+      } else {
+        return `Error: Divisor cannot be zero`
+      }
+    }
+
+    remainder(x, y) {
+      if (y !== 0) {
+        return x % y;
+      } else {
+        return `Error: Divisor cannot be zero`
+      }
+    }
+
+    elevate(x, y) {
+      return x ** y;
+    }
+
+    sqrt(x) {
+      if (x > 0) {
+        return Math.sqrt(x);
+      } else {
+        return `Error: Cannot calculate the square root of a negative number.`
+      }
     }
 }
+
 const calc=new Calculator()
+
+//console.log(calc.ratio(5, 7, 300));
+//console.log(calc.percentage(3, 12)); 
+//console.log(calc.add(5, 7)); 
+//console.log(calc.subtract(5, 7)); 
+console.log(calc.multiply(5, 7)); 
+console.log(calc.divide(35, 7)); 
+console.log(calc.remainder(7, 5)); 
+console.log(calc.elevate(5, 3)); 
+console.log(calc.sqrt(25));
+
+
+
